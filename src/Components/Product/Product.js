bcaf59e375384dc282a8f4e554ff4705d778a1a2
Mokadderul Islam <nahidnahid63@gmail.com>
2021-10-13 00:43:34 +0600

    third

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -16,7 +16,7 @@ const Product = (props) => {
                  <p><small>by:{seller}</small></p>
                  <p>${price}</p>
                  <p><small>Only {stock} left in stock - Order soon</small></p>
-                 <button className="main-button"> <FontAwesomeIcon icon={faShoppingCart} />Add to cart </button>
+                 <button className="main-button" onClick={()=>props.handleAddProduct(props.product)}> <FontAwesomeIcon icon={faShoppingCart} />Add to cart </button>
             </div>
         </div>
     );
